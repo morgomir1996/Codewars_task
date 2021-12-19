261c901a52360d7e7b9749796c5bee5aa7cd010b
Oleksandr Omelchuk <51052073+morgomir1996@users.noreply.github.com>
2021-12-19 16:13:23 +0200

    Update codewarsTask_2.js

--- codewarsTask_2.js
+++ codewarsTask_2.js
@@ -1,16 +1,16 @@
 // https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript
 
-
 function pigIt(str){
 let newStr = str.split(' ')
 let pigIt = "";
 for (i = 0; i < newStr.length; i++) {
     let currentWord = newStr[i];
+    if (currentWord.length > 1) {
         if (i != 0) {
           pigIt += " " + currentWord.slice(1, (currentWord.length)) + currentWord[0] + "ay";
           } 
-          else { pigIt += currentWord.slice(1, (currentWord.length)) + currentWord[0] + "ay"; }
-
+          else { pigIt += currentWord.slice(1, (currentWord.length)) + currentWord[0] + "ay"; } 
+    } else {pigIt += currentWord}
 }
 return pigIt; 
 }
