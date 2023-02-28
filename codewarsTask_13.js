3071a4708ad68931b70676cdec77c0b8fe98125a
Oleksandr Omelchuk <51052073+morgomir1996@users.noreply.github.com>
2023-02-28 20:03:10 +0200

    Update codewarsTask_13.js

--- codewarsTask_13.js
+++ codewarsTask_13.js
@@ -5,3 +5,18 @@ function narcissistic(value) {
   const digits = str.split("").map((x) => Math.pow(x, str.length)).reduce((a, b) => a + b);
   return Number(digits) === value;
 }
+
+
+//   https://www.codewars.com/kata/58b972cae826b960a300003e/train/javascript
+
+
+function missingWord(nums, str) {
+  const positions = nums.sort((a, b) => a - b);
+  const letters = str.toLowerCase().split("").reduce((a, b) => (b !== " " ? a + b : a));
+  const word = positions.map((x) => (x = letters[x])).join("");
+  if (word.length === 3) {
+    return word;
+  } else {
+    return "No mission today";
+  }
+}
